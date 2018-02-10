d0fccc3fab94eebb0f9e34e3bd3f0cb49647e583
Ravindra Prasad <ravindraprasad@yahoo.com>
2018-02-10 23:48:09 +0530

    Update dev.py

--- teamtemp/settings/dev.py
+++ teamtemp/settings/dev.py
@@ -7,14 +7,22 @@ SECRET_KEY = os.environ.get(
     'TEAMTEMP_SECRET_KEY',
     'rp47vufz8lrr1cxki7lmc9w221ajgauk5ctv6xj')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(PROJECT_DIR, 'teamtemp.postgresql'),
-    }
-}
+
+if ON_HEROKU:
+    DATABASE_URL = 'postgresql:///postgresql'
+else:
+    DATABASE_URL = 'sqlite://' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': os.path.join(PROJECT_DIR, 'teamtemp.postgresql'),
+#    }
+# }
 
 # Update database configuration with $DATABASE_URL.
-DATABASES['default'].update(
-    dj_database_url.config(conn_max_age=600)
-)
+# DATABASES['default'].update(
+#    dj_database_url.config(conn_max_age=600)
+# )
