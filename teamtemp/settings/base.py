4639fc7b3471b77383a6895f85b9b6cd786a1322
Ravindra Prasad <ravindraprasad@yahoo.com>
2018-02-10 15:43:21 +0530

    Update base.py

--- teamtemp/settings/base.py
+++ teamtemp/settings/base.py
@@ -84,7 +84,7 @@ STATICFILES_FINDERS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
